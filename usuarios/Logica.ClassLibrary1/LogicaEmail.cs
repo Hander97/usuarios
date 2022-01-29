3bfdc45f79a1d78a6656ec25602b392c2d6f8aa0
HANDER DAVID GUILLEN ARTEAGA <hander.guillen.arteaga@estudiantes.cordillera.edu.ec>
2022-01-29 12:59:42 -0500

    Smtp

---
+++ usuarios/Logica.ClassLibrary1/LogicaEmail.cs
@@ -0,0 +1,29 @@
+﻿using AccesoDatos.ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.ClassLibrary1
+{
+    public class LogicaEmail
+    {
+        private static DcMantenimientoDataContext dc = new DcMantenimientoDataContext();
+
+        public static Smtp getConfigEmailServer()
+        {
+            try
+            {
+                var stmpData = dc.Smtp.FirstOrDefault(data => data.smtp_status == 'A'
+                                                       && data.smtp_codigo.Equals(1));
+
+                return stmpData;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Error al obtener Smtp " + ex.Message);
+            }
+        }
+    }
+}
