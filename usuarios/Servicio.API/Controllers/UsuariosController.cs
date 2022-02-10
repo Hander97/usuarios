67b1ad28d7eaf82010f1c5f3aed52e5ca6861567
HANDER DAVID GUILLEN ARTEAGA <hander.guillen.arteaga@estudiantes.cordillera.edu.ec>
2022-02-10 12:11:53 -0500

    apis endpoint

---
+++ usuarios/Servicio.API/Controllers/UsuariosController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Servicio.API.Models;
+
+namespace Servicio.API.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private DBMATRICULAEntities db = new DBMATRICULAEntities();
+
+        public IQueryable<Usuario> GetUsuario()
+        {
+            return db.Usuario;
+        }
+
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult GetUsuario(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUsuario(int id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.usu_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Usuarios
+        //[ResponseType(typeof(Usuario))]
+        //public IHttpActionResult PostUsuario(Usuario usuario)
+        //{
+          //  if (!ModelState.IsValid)
+            //{
+              //  return BadRequest(ModelState);
+            //}
+
+            //db.Usuario.Add(usuario);
+            //db.SaveChanges();
+
+            //return CreatedAtRoute("DefaultApi", new { id = usuario.usu_id }, usuario);
+        //}
+
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult DeleteUsuario(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return db.Usuario.Count(e => e.usu_id == id) > 0;
+        }
+    }
+}
